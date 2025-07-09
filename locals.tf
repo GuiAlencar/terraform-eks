c5be0a5b555d403d248abb5117a3bb419b3f5d73
guiAlencar <uchihaguilherme4@gmail.com>
2025-07-08 22:03:30 -0300

    adiciona config vpc

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals {
+  tags = {
+    Department   = "DevOPs"
+    Organization = "Infrastructure and Operations"
+    Project      = "EKS"
+    Environment  = "Development"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
 variable "cidr_block" {
-  type = string
+  type        = string
   description = "Networking CIDR block to be used for the VPC"
+}
+
+variable "project_name" {
+  type = string 
+  description = "Project name to be used to name the resources (Name tag)"
 }
